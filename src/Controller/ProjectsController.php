b0c57c697e73eca00470f1fa28de9dee9a847bf5
idlbi3u <idlbi3u@etu.univ-lorraine.fr>
2023-02-25 13:22:09 +0100

    details page update

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectsController extends AbstractController
 {
+
+    public function __construct
+    (
+        private EntityManagerInterface $em,
+        private ProjectRepository $projectRepository
+    )
+    {
+        $this->em = $em;
+        $this->projectRepository = $projectRepository;
+    }
 
     #[Route('/projects', name: 'projects_page')]
     public function index(): Response
@@ -18,10 +30,12 @@ class ProjectsController extends AbstractController
     }
 
     #[Route('/projects/{id}', name: 'projects_details_page')]
-    public function projectDetails(): Response
-    {        
+    public function projectDetails(int $id): Response
+    {  
+        $project = $this->projectRepository->find($id);
         return $this->render('details/projects-details.html.twig', [
-            'current_route' => 'projects_details_page'
+            'current_route' => 'projects_details_page',
+            'project' => $project
         ]);
     }
 }
